0775d3861bd3644db8b4a5578fa2b2946635bdb9
hasanmiaweb <hasanmiaweb@gmail.com>
2021-02-08 02:35:11 +0600

    simple Update

--- js/app.js
+++ js/app.js
@@ -1,61 +1,57 @@
-// All common variable
+//Search for Meal
 
-const searchBtn = document.getElementById("search");
-const searchinput = document.getElementById("searchinput");
-const searchProductResult = document.getElementById("searchProductResult");
-const searchMainbar = document.getElementById("searchMainbar");
+const searchButton = async () => {
+  const searchinput = document.getElementById("searchinput");
+  apiUrl = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchinput.value}`;
+  const response = await fetch(apiUrl);
+  const data = await response.json();
+  desplayMeals(data.meals);
+};
 
-// Search Button fatch And addEventListener
-searchBtn.addEventListener("click", function () {
-  fetch(
-    `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchinput.value}`
-  )
-    .then((response) => response.json())
-    .then((data) => showProduct(data.meals));
-});
+// Desplay Search Result
 
-const showProduct = (showProductAll) => {
+const desplayMeals = (showProductAll) => {
   let htmlTemplate = "";
   if (showProductAll) {
-    showProductAll.forEach((showProductDetails) => {
+    showProductAll.forEach((showmeals) => {
       htmlTemplate =
         htmlTemplate +
-        `
-      <div class="col-md-3">
-        <span onclick="productDes(${showProductDetails.idMeal})"  id="product">
-          <div class="productShowInner">
-            <div class="productImg">
-              <img
-                id="productphoto"
-                src="${showProductDetails.strMealThumb}"
-                class="img-fluid"
-                alt=""
-              />
+        `<div class="col-md-3">
+          <span onclick="productDes(${showmeals.idMeal})"  id="product">
+            <div class="productShowInner">
+              <div class="productImg">
+                <img
+                  id="productphoto"
+                  src="${showmeals.strMealThumb}"
+                  class="img-fluid"
+                  alt=""
+                />
+              </div>
+              <div class="productName text-center">
+                <p>${showmeals.strMeal}</p>
+              </div>
             </div>
-            <div class="productName text-center">
-              <p>${showProductDetails.strMeal}</p>
-            </div>
-          </div>
-        </span>
-      </div>
-        `;
+          </span>
+        </div>`;
     });
   } else {
-    htmlTemplate = "Data not found";
-    document.getElementById("searchProductResult").innerHTML = htmlTemplate;
+    // htmlTemplate = "Data not found";
+    htmlTemplate = document.getElementById(
+      "error"
+    ).innerHTML = `<div id="errorMessege" ><i class="fas fa-exclamation-triangle"></i> <h2>Oops..!</h2>
+    <span>DATA NOT FOUND </span>
+    </div>`;
+    document.getElementById("searchProduct").innerHTML = htmlTemplate;
   }
-
-  document.getElementById("searchProductResult").innerHTML = htmlTemplate;
+  document.getElementById("searchProduct").innerHTML = htmlTemplate;
 };
 
 // Single Product Details
-const productDes = (singleProductDetails) => {
-  const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${singleProductDetails}`;
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      productFullInfo(data.meals[0]);
-    });
+const productDes = async (singleProductDetails) => {
+  const apiUrl = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${singleProductDetails}`;
+  const response = await fetch(apiUrl);
+  const data = await response.json();
+  productFullInfo(data.meals[0]);
 };
 
 const productFullInfo = (singleProductInfo) => {
@@ -72,27 +68,24 @@ const productFullInfo = (singleProductInfo) => {
 
   let htmlTemplate = `
   <div class="col-md-6 m-auto">
-  <h3 id="searchResultTitle">Food Details</h3>
-  <div class="shoProductDetailsInner mt-3">
-    <img src="${singleProductInfo.strMealThumb}" class="img-fluid" alt="" />
-    <div class="shoProductDetailsTitle">
-      <h2>${singleProductInfo.strMeal}</h2>
-      <h4>Ingredients</h4>
-
-      <ul id="detailsLi">
-      ${singleProductadd
-        .map(
-          (liContent) =>
-            `<li><i class="fas fa-check-square"></i><span> ${liContent}</span></li>`
-        )
-        .join("")}
-      </ul>
-      <hr>
+    <h3 id="searchResultTitle">Food Details</h3>
+    <div class="shoProductDetailsInner mt-3">
+      <img src="${singleProductInfo.strMealThumb}" class="img-fluid" alt="" />
+      <div class="shoProductDetailsTitle">
+        <h2>${singleProductInfo.strMeal}</h2>
+        <h4>Ingredients</h4>
+  
+        <ul id="detailsLi">
+        ${singleProductadd
+          .map(
+            (liContent) =>
+              `<li><i class="fas fa-check-square"></i><span> ${liContent}</span></li>`
+          )
+          .join("")}
+        </ul>
+        <hr>
+      </div>
     </div>
-  </div>
-</div>`;
-
-  // searchProductResult.classList.add("d-none");
-  // searchMainbar.classList.add("d-none");
+  </div>`;
   document.getElementById("singleProductDetails").innerHTML = htmlTemplate;
 };
